8b13ebd7d3ea912a8cbb80ff8495937d194a6896
Yashwant Jagdale <yashwant.jagdale@veritas.com>
2018-08-07 15:13:02 +0530

    Parallel Test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,34 +9,39 @@ pipeline {
 
         stage('Build with unit testing') {
             steps {
-                // Run the maven build
-                script {
-                    // Get the Maven tool.
-                    // ** NOTE: This 'M3' Maven tool must be configured
-                    // **       in the global configuration.
-                    echo 'Pulling...' + env.BRANCH_NAME
-                    def mvnHome = tool 'Maven 3.3.9'
-                    if (isUnix()) {
-                        def targetVersion = getDevVersion()
-                        print 'target build version...'
-                        print targetVersion
-                        sh "'${mvnHome}/bin/mvn' -DskiptTest=false -Dbuild.number=${targetVersion} clean package"
-                        def pom = readMavenPom file: 'pom.xml'
-                        // get the current development version
-                        developmentArtifactVersion = "${pom.version}-${targetVersion}"
-                        print pom.version
-                        // execute the unit testing and collect the reports
-                        junit '**//*target/surefire-reports/TEST-*.xml'
-                        archiveArtifacts 'target*//*.jar'
-                    } else {
-                        bat(/"${mvnHome}\bin\mvn" -Dintegration-tests.skip=true clean package/)
-                        def pom = readMavenPom file: 'pom.xml'
-                        print pom.version
-                        junit '**//*target/surefire-reports/TEST-*.xml'
-                        archive 'target*//*.jar'
+                parallel {
+                  stage ('clean project') {
+                      script {
+                          // Get the Maven tool.
+                          // ** NOTE: This 'M3' Maven tool must be configured
+                          // **       in the global configuration.
+                          echo 'Pulling...' + env.BRANCH_NAME
+                          def mvnHome = tool 'Maven 3.3.9'
+                          if (isUnix()) {
+                              def targetVersion = getDevVersion()
+                              print 'target build version...'
+                              print targetVersion
+                              sh "'${mvnHome}/bin/mvn' -DskiptTest=false -Dbuild.number=${targetVersion} clean package"
+                              def pom = readMavenPom file: 'pom.xml'
+                              // get the current development version
+                              developmentArtifactVersion = "${pom.version}-${targetVersion}"
+                              print pom.version
+                              // execute the unit testing and collect the reports
+                              junit '**//*target/surefire-reports/TEST-*.xml'
+                              archiveArtifacts 'target*//*.jar'
+                          } else {
+                              bat(/"${mvnHome}\bin\mvn" -Dintegration-tests.skip=true clean package/)
+                              def pom = readMavenPom file: 'pom.xml'
+                              print pom.version
+                              junit '**//*target/surefire-reports/TEST-*.xml'
+                              archive 'target*//*.jar'
+                          }
+                      }
+                  }
+                    stage ('Run print statements') {
+                        sh "echo 'waiting for completiong'; sleep 60;"
                     }
                 }
-
             }
         }
     }
