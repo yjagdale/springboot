7c13064582f50bfa140b297db01008b1698f5514
Yashwant Jagdale <yashwant.jagdale@veritas.com>
2018-08-07 14:41:21 +0530

    Test cases added

---
+++ src/test/java/com/tests/SimpleTestCases.java
@@ -0,0 +1,58 @@
+package com.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleTestCases {
+
+    @Test
+    public void testcase1 () {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testcase2 () {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testcase3 () throws InterruptedException {
+        Assert.assertTrue(true);
+        Thread.sleep(4000);
+    }
+
+    @Test
+    public void testcase4 () {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testcase5 () {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testcase6 () {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testcase7 () {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testcase8 () {
+        Assert.assertTrue(true);
+        System.out.println("execution");
+    }
+    @Test
+    public void testcase9 () {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void testcase10 () {
+        Assert.assertTrue(true);
+    }
+}
